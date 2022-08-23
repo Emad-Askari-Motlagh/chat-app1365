ef18da63ec44b602d010126abaa8e8e13703a00a
Emad Askari <emad.askari@gmail.com>
2022-08-23 21:27:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://chat-app1365.vercel.app",
+    origin: "wss://chat-app1365.vercel.app",
     methods: ["GET", "POST"],
   },
 });
