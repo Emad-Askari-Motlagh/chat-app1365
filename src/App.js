0969d4d39dcebd09d68c67d0f82a926eef38d1f2
Emad Askari <emad.askari@gmail.com>
2022-08-23 21:27:17 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
 
-const socket = io.connect("https://chat-app1365.vercel.app");
+const socket = io.connect("wss://chat-app1365.vercel.app");
 
 function App() {
   const [username, setUsername] = useState("");
