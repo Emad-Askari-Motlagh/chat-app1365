7455cb88ba940ecf67699cd912d2deaba6bf4683
Emad Askari <emad.askari@gmail.com>
2022-08-24 12:17:26 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const express = require("express");
-const app = express();
-const http = require("http");
-const cors = require("cors");
-const { Server } = require("socket.io");
-app.use(cors());
+const { createServer } = require('http')
+const { parse } = require('url')
+const next = require('next')
 
-const server = http.createServer(app);
+const dev = process.env.NODE_ENV !== 'production'
+const hostname = 'localhost'
+const port = 3001
+// when using middleware `hostname` and `port` must be provided below
+const app = next({ dev, hostname, port })
+const handle = app.getRequestHandler()
 
-const io = new Server(server, {
+app.prepare().then(() => {
+  createServer(async (req, res) => {
+
+    try {
+      // Be sure to pass `true` as the second argument to `url.parse`.
+      // This tells it to parse the query portion of the URL.
+      const parsedUrl = parse(req.url, true)
+      const { pathname, query } = parsedUrl
+const io = new Server(3001, {
   cors: {
     origin: "wss://chat-app1365.vercel.app",
     methods: ["GET", "POST"],
@@ -30,6 +40,37 @@ io.on("connection", (socket) => {
     console.log("User Disconnected", socket.id);
   });
 });
+
+      if (pathname === '/a') {
+        await app.render(req, res, '/a', query)
+      } else if (pathname === '/b') {
+        await app.render(req, res, '/b', query)
+      } else {
+        await handle(req, res, parsedUrl)
+      }
+    } catch (err) {
+      console.error('Error occurred handling', req.url, err)
+      res.statusCode = 500
+      res.end('internal server error')
+    }
+  }).listen(port, (err) => {
+    if (err) throw err
+    console.log(`> Ready on http://${hostname}:${port}`)
+  })
+})
+
+
+
+const express = require("express");
+const app = express();
+const http = require("http");
+const cors = require("cors");
+const { Server } = require("socket.io");
+app.use(cors());
+
+const server = http.createServer(app);
+
+
 
 server.listen(3001, () => {
   console.log("SERVER RUNNING");
